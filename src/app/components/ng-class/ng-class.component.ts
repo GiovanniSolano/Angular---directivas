93b5115e512f104a049f0dd55cbf6e4e236cdf83
Giovanni Solano <giovannisolano211@gmail.com>
2021-02-02 20:21:18 -0600

    Directiva ngClass + ejemplo

--- src/app/components/ng-class/ng-class.component.ts
+++ src/app/components/ng-class/ng-class.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgClassComponent implements OnInit {
 
-  constructor() { }
+  cadenaClases: string;
+  arrayClases: string[];
+
+  colorSelected: string;
+
+  constructor() {
+    this.cadenaClases = 'redondo rojo';
+    this.arrayClases = ['semaforo'];
+    this.arrayClases.push('rojo');
+    this.arrayClases.push('redondo');
+
+    this.colorSelected = 'v';
+
+   }
 
   ngOnInit(): void {
+
+    setInterval(() => {
+
+      if(this.colorSelected === 'r') {
+        this.colorSelected = 'a';
+      } else if (this.colorSelected === 'a') {
+        this.colorSelected = 'v';
+      } else if (this.colorSelected === 'v') {
+        this.colorSelected = 'r';
+      }
+
+    },2000);
+
   }
 
 }
